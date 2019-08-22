3968ca1f1bb1cd79b24ba5cd206ff525d1355c39
Takumikat <52455003+Takumikat@users.noreply.github.com>
2019-08-22 22:01:01 +0200

    Delete script.js

--- script.js
+++
@@ -1,8 +0,0 @@
-function multiply(inputNumber1, inputNumber2) {
-  let outputNumber = inputNumber1 * inputNumber2;
-  return outputNumber;
-}
-
-console.log(multiply(2, 3));
-console.log(multiply(2, multiply(2,3)));
-console.log(outputNumber)
